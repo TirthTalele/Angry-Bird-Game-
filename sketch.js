81aaeb82dc06b3a77dfeb14daca6ad258e78f24f
TirthTalele <72204350+TirthTalele@users.noreply.github.com>
2020-12-15 17:35:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,153 @@
+var num = 4;
+console.log(num); 
+
+var name = "tirth";
+console.log(name);
+
+var bool = true;
+console.log(bool);
+
+var nothing = null;
+console.log(nothing);
+
+var score;
+console.log(score);
+
+var arr1 = [1,2,3,4,5];
+
+console.log(arr1);
+console.log(arr1[4]);
+console.log(arr1[0]);
+
+var arr2 = [32,"tirth",nothing];
+
+console.log(arr2);
+
+var arr3 = [[1,2],[3,4],[5,6]];
+console.log(arr3[0]);
+console.log(arr3[1][1]);
+console.log(arr3[2][0]);
+ 
+arr1.push(6,7);
+console.log(arr1);
+
+arr1.pop();
+console.log(arr1);
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+var gamestate = "onsling";
+var score = 0;
+function preload() {
+    bground();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+   
+     platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+    
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+}
+
+function draw(){
+    if (backgroundImg){
+    background(backgroundImg);
+    }
+    textSize(25)
+    text("Score: "+ score,900,50);
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    pig1.score() 
+    log1.display();
+     pig3.score();
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+    
+    bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    if (gamestate!=="launched"){
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gamestate = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32 && bird.body.speed < 1){
+        Matter.Body.setPosition(bird.body, {x: 200 , y: 50});
+      slingshot.attach(bird.body);
+      gamestate = "onsling";
+        bird.trajectory= [];
+    }
+}
+
+ async function bground(){
+
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+
+    var responsejson = await response.json();
+
+    var dt = responsejson.datetime;
+
+    var hour = dt.slice(11,13);
+    console.log(hour) ;
+  if(hour>=06 && hour <=19){
+
+    bgr = "sprites/bg.png"
+  }else {
+
+    bgr = "sprites/bg2.jpg"
+  }
+
+    backgroundImg = loadImage(bgr);
+} 
